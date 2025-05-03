3b03debe426d2744343611b3da95a7582745a1b9
Maillard Paul <92569066+AuriLeBG@users.noreply.github.com>
2025-05-03 10:16:29 +0200

    clean

--- backend/src/Extern/TMDbApi.hs
+++ backend/src/Extern/TMDbApi.hs
@@ -58,16 +58,3 @@ getOutMovie title = do
              return movies
        Nothing  -> return []
   
--- type OMDbAPI = QueryParam' '[Required] "apikey" Text
---                    :> QueryParam "t" Text
---                    :> Get '[JSON] MovieExtern
-
--- omdbClient :: Text -> Maybe Text -> ClientM MovieExtern
--- omdbClient = client (Proxy :: Proxy OMDbAPI)
-
--- fetchMovie :: Text -> Text -> IO (Either ClientError MovieExtern)
--- fetchMovie apiKey title = do
---     manager <- newManager tlsManagerSettings
---     let env = mkClientEnv manager (BaseUrl Https "www.omdbapi.com" 443 "")
---     runClientM (omdbClient apiKey (Just title)) env
-
